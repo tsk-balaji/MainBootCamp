5369a8d3691e4c52fe2bb377ef1f0d3a78ccbf15
tsk-balaji <tskbalaji15@gmail.com>
2024-02-27 00:41:22 +0530

    commit-task 3

--- Tasks/Tasks/Task 3/countriesFlag.js
+++ Tasks/Tasks/Task 3/countriesFlag.js
@@ -2,6 +2,14 @@
   const req = new XMLHttpRequest();
   req.open("GET","https://restcountries.com/v3.1/all");
   req.send();
-  req.onload = fucntion () {
-    console.log(JSON.parse(this.response));
-   }
+  req.onload = function () {
+    const obj = JSON.parse(this.response);
+    for (let i = 0; i < obj.length; i++) {
+      //flags of the countries
+      console.log(obj[i].flags.png);   //flags
+      console.log(obj[i].region);      //region
+      console.log(obj[i].subregion);   //subregion
+      console.log(obj[i].population);  //population
+      console.log(obj[i].name.common); //countryNames
+    }
+  }
