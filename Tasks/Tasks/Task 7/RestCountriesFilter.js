2f75a20779c1254276de8d05219563822e1b5c79
tsk-balaji <tskbalaji15@gmail.com>
2024-03-11 14:50:19 +0530

    Task 7

--- Tasks/Tasks/Task 7/RestCountriesFilter.js
+++ Tasks/Tasks/Task 7/RestCountriesFilter.js
@@ -5,11 +5,29 @@
   req.onload = function () {
     const obj = JSON.parse(this.response);
     console.log(obj);
+
+    //Asian countries
     asianCountries = obj.filter((element) => element.region == "Asia")
     console.log(asianCountries);
+    
+    //Countries with population less than 2L.
     countriesWithPopLessThan2L = obj.filter((element) => element.population < 200000)
     console.log(countriesWithPopLessThan2L);
-    countiresUsingUSD = obj.filter((elem) => elem.currencies === "USD")
-    console.log(countiresUsingUSD);
 
+    //For Each - Name, Capital, Flag,
+
+    obj.forEach(element => {
+      console.log(element.name.common);
+      console.log(element.capital);
+      console.log(element.flags.png);
+      
+    });
+
+    //Total population.
+    totalPopulation = obj.reduce((acc, country) => acc + country.population , 0)
+    console.log(totalPopulation);
+
+    //Countries using USD currency.
+    countiresUsingUSD = obj.filter((country) => country.currencies && country.currencies.USD)
+    console.log(countiresUsingUSD);
   }
